86c0726d80f1dd5ea8b7eefea61b3e5ffcc22515
Chandera-Raghav <raghavchandera@gmail.com>
2024-11-07 10:09:08 +0530

    change image, name, time and massege counting

--- lib/whatsapp_body_page.dart
+++ lib/whatsapp_body_page.dart
@@ -11,67 +11,93 @@ class _WhatsAppBodyAppState extends State<WhatsAppBodyPage> {
     {
       "image" : "assets/images/download8.png",
       "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:15AM",
+      "mnum" : "4"
+    },
+    {
+      "image" : "assets/images/download.jpg",
+      "name" : "Hari Guapte",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:12AM",
+      "mnum" : "6"
+    },
+    {
+      "image" : "assets/images/download18.jpg",
+      "name" : "Roma Gadhitya",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:12AM",
+      "mnum" : "1"
+    },
+    {
+      "image" : "assets/images/download19.png",
+      "name" : "Jignesh Rawat",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:12AM",
+      "mnum" : "8"
+    },
+    {
+      "image" : "assets/images/download12.png",
+      "name" : "jiya Dhumani",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:11AM",
+      "mnum" : "2"
+    },
+    {
+      "image" : "assets/images/download11.png",
+      "name" : "Tarun Rana",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:11AM",
+      "mnum" : "3"
+    },
+    {
+      "image" : "assets/images/download10.png",
+      "name" : "Soniya Chobay",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:10AM",
+      "mnum" : "5"
+    },
+    {
+      "image" : "assets/images/images1.png",
+      "name" : "Jay Shukla",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:09",
+      "mnum" : "6"
+    },
+    {
+      "image" : "assets/images/images2.jpg",
+      "name" : "Gomit Guru",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:09AM",
+      "mnum" : "4"
+    },
+    {
+      "image" : "assets/images/images3.jpg",
+      "name" : "Jigna Maru",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:09AM",
+      "mnum" : "9"
     },
     {
       "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
+      "name" : "Kan solanki",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "11:08AM",
+      "mnum" : "5"
     },
     {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
+      "image" : "assets/images/download19.png",
+      "name" : "Jaysukh Swamy",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "10:09AM",
+      "mnum" : "7"
     },
     {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
-    },
-    {
-      "image" : "assets/images/download8.png",
-      "name" : "Gomit Guru",
-      "msg" : "Hi!, Raghav how are you."
+      "image" : "assets/images/download10.png",
+      "name" : "Girimani Aparnathi",
+      "msg" : "Hi!, Raghav how are you.",
+      "time" : "10:00AM",
+      "mnum" : "2"
     }
   ];
   @override
@@ -106,8 +132,8 @@ class _WhatsAppBodyAppState extends State<WhatsAppBodyPage> {
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text(element["name"], style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
-                                Text(element["msg"], style: TextStyle(fontSize: 20, color: Colors.white),)
+                                Text(element["name"], style: const TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
+                                Text(element["msg"], style: const TextStyle(fontSize: 20, color: Colors.white),)
                               ],
                             ),
                           ),
@@ -118,7 +144,7 @@ class _WhatsAppBodyAppState extends State<WhatsAppBodyPage> {
                     margin: const EdgeInsets.all(0),
                     child: Column(
                       children: [
-                        const Text("10:00AM", style: TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),),
+                        Text(element["time"], style: const TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),),
                         Container(
                           margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
                           width: 22,
@@ -127,7 +153,7 @@ class _WhatsAppBodyAppState extends State<WhatsAppBodyPage> {
                               shape: BoxShape.circle,
                               color: Colors.greenAccent
                           ),
-                          child: const Center(child: Text("4", style: TextStyle(fontSize: 18, color: Colors.black),)),
+                          child: Center(child: Text(element["mnum"], style: const TextStyle(fontSize: 18, color: Colors.black),)),
                         )
                       ],
                     ),
